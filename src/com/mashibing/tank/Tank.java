44d34450d232f3b43f2604376aa56b0712abaafa
yuanzt <yzt0313@163.com>
2021-03-04 15:17:02 +0800

    抽象出tank类，封装期属性方法，在TankFrame中直接调用。

---
+++ src/com/mashibing/tank/Tank.java
@@ -0,0 +1,70 @@
+package com.mashibing.tank;
+
+import java.awt.Graphics;
+
+public class Tank {
+
+	private int x,y;
+	private Dir dir = Dir.DOWN;
+	private static final int speed = 10;
+	
+	public Tank() {
+	}
+
+	public Tank(int x, int y, Dir dir) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.dir = dir;
+	}
+
+	public void paint(Graphics g) {
+		g.fillRect(x, y, 50, 50);
+		switch (dir) {
+		case LEFT:
+			x -= speed;
+			break;
+		case UP:
+			y -= speed;
+			break;
+		case RIGHT:
+			x += speed;
+			break;
+		case DOWN:
+			y += speed;
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public Dir getDir() {
+		return dir;
+	}
+
+	public void setDir(Dir dir) {
+		this.dir = dir;
+	}
+
+	public static int getSpeed() {
+		return speed;
+	}
+	
+}
