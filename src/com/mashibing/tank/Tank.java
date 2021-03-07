c97a873c74a9b00a1ca8bf7eeaa7cd78334ffa12
yzt_glb <yzt_glb@123.com>
2021-03-07 17:14:09 +0800

    区别己方坦克和敌方坦克图片区分

--- src/com/mashibing/tank/Tank.java
+++ src/com/mashibing/tank/Tank.java
@@ -17,7 +17,7 @@ public class Tank {
 	
 	private Group group = Group.BAD;
 	
-	public static int WIDTH = ResourceMgr.tankD.getWidth(),HEIGHT = ResourceMgr.tankD.getHeight();
+	public static int WIDTH = ResourceMgr.goodTankU.getWidth(),HEIGHT = ResourceMgr.goodTankU.getHeight();
 	
 	public Tank() {
 	}
@@ -37,16 +37,16 @@ public class Tank {
 		
 		switch (dir) {
 		case LEFT:
-			g.drawImage(ResourceMgr.tankL,x,y,null);
+			g.drawImage(this.group == Group.GOOD ? ResourceMgr.goodTankL : ResourceMgr.badTankL,x,y,null);
 			break;
 		case UP:
-			g.drawImage(ResourceMgr.tankU,x,y,null);
+			g.drawImage(this.group == Group.GOOD ? ResourceMgr.goodTankU : ResourceMgr.badTankU,x,y,null);
 			break;
 		case RIGHT:
-			g.drawImage(ResourceMgr.tankR,x,y,null);
+			g.drawImage(this.group == Group.GOOD ? ResourceMgr.goodTankR : ResourceMgr.badTankR,x,y,null);
 			break;
 		case DOWN:
-			g.drawImage(ResourceMgr.tankD,x,y,null);
+			g.drawImage(this.group == Group.GOOD ? ResourceMgr.goodTankD : ResourceMgr.badTankD,x,y,null);
 			break;
 
 		default:
